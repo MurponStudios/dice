7054a76094323430096459dbdc3b9895bfe5408c
MurponStudios <132826356+MurponStudios@users.noreply.github.com>
2023-05-07 15:22:06 +0100

    Оновити main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let random_num = 0
 input.onGesture(Gesture.Shake, function () {
-    random_num = randint(1, 6)
-    basic.showNumber(random_num)
+    for (let index = 0; index < 1; index++) {
+        random_num = randint(1, 6)
+        basic.showNumber(random_num)
+    }
 })
 basic.forever(function () {
 	
